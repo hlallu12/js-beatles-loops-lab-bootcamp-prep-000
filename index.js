042bfc97996151b57fa5fa89275f571df680d59f
hansalallu@gmail.com <hansalallu@gmail.com>
2018-08-26 20:31:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(musicians, instrument){
+var empty=[]
+for(let i=0; i <= musicians.length-1; i++){
+ empty.push(musicians[i]+ " plays "+ instrument[i])
+}
+return empty
+}
+
+function johnLennonFacts(facts){
+  var length = facts.length  
+  while(length>=0){
+    facts[length]= facts[length]+ " !!!" 
+    length--
+  }
+  return facts
+}
